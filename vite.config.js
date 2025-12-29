12c441f266f0a4aa46bd2ddf363564e82ac04957
Smriti Bhadouriya <smriti.marcadeo@gmail.com>
2025-12-29 11:20:26 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  server: {
+    mimeTypes: {
+      'jsx': 'text/javascript',  // Explicitly map .jsx to JS MIME for module scripts
+    },
+  },
 })
